c5b7e7a2249a6a2b079184aeb3a813f63d67faaa
Konrad Gibaszewski <konrad@syncube.de>
2013-06-19 13:44:32 +0200

    initProject.sh - refactored copy action into a function

--- initProject.sh
+++ initProject.sh
@@ -2,12 +2,12 @@
 
 # Simple bash script to copy default file into it's target positions.
 #
-# @toDo Completion statuses come on the end of the lines
-# @toDo Git messages are shown only on error
-# @toDo Confirmation for copy action of each files
-# @toDo Done. Overwrite check for files
-# @toDo Propose direct edit action for each file copied (answer with y/N)
-
+# Licensed under The MIT License
+# Redistributions of files must retain the above copyright notice.
+#
+# @toDo         https://github.com/syncube/cakeProjectSkeleton/issues
+# @copyright    Copyright (c) 2013, Konrad Gibaszewski
+# @license      http://www.opensource.org/licenses/mit-license.php The MIT License
 
 # Files to be copied from defaults
 #
@@ -19,6 +19,20 @@
 # app/webroot/robots.txt
 # app/webroot/repoData/config/config.ini
 
+
+# copyFile sourceFileName destinationFileName
+#
+# Copies source file into it's destination and asks if you want to edit it with nano
+#
+function copyFile {
+
+    cp -pvi $1 $2
+    read -p "Would you like to edit $2 (y/n [n])?"
+    ([ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]) && nano $2
+
+}
+
+
 # Init git submodules
 echo -e "\n\033[1;36m Git submodules\033[0m intialising...";
 git submodule init
@@ -29,20 +43,20 @@ echo -e "\n\033[1;36m Git submodules\033[0m intialising...\033[1;32m OK\033[0m";
 # Init app (copy defaults to its target positions)
 echo -e "\n\033[1;36m Application config files\033[0m intialising...";
 cd app/
-cp -pvi .htaccess.default .htaccess
+copyFile .htaccess.default .htaccess
 cd Config/
-cp -pvi core.default.php core.php
-cp -pvi database.default.php database.php
-cp -pvi email.default.php email.php
+copyFile core.default.php core.php
+copyFile database.default.php database.php
+copyFile email.default.php email.php
 cd ../webroot/
-cp -pvi .htaccess.default .htaccess
-cp -pvi robots.default.txt robots.txt
+copyFile .htaccess.default .htaccess
+copyFile robots.default.txt robots.txt
 echo -e "\n\033[1;36m Application config files\033[0m intialising...\033[1;32m OK\033[0m";
 
 # Init git auto pull
 echo -e "\n\033[1;36m Git auto pull config file\033[0m intialising...";
 cd repoData/config/
-cp -pvi config.ini.default config.ini
+copyFile config.ini.default config.ini
 echo -e "\n\033[1;36m Git auto pull config file\033[0m intialising...\033[1;32m OK\033[0m";
 
 # Exit msg
