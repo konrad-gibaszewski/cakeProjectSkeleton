1dde2acdb6d27f4ccfd5aef69dcd9869bcdb7033
Konrad Gibaszewski <konrad@syncube.de>
2013-07-09 14:28:30 +0200

    PHPTal custom View as submodule

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,5 +31,13 @@ App::uses('Controller', 'Controller');
  * @package		app.Controller
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
-class AppController extends Controller {
+class AppController extends Controller
+{
+
+    function beforeFilter()
+    {
+        parent::beforeFilter();
+        $this->viewClass = 'Phptal';  // Use custom PHPTal View
+    }
+
 }
